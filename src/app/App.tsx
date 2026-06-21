76624fc4a3d4070c380252242c330ff26f8619e8
Shampoo-cow <songhyun9191@gmail.com>
2026-06-21 12:41:24 +0900

    Remove ledger page, simplify App to single GuildMarkPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { GuildMarkPage } from "./components/GuildMarkPage";
-import { BlacksmithLedgerPage } from "./components/BlacksmithLedgerPage";
 import faviconImage from "figma:asset/cb0f5c1c966b5decd0275b09e80838bc724c6eac.png";
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState<'guildmarks' | 'ledger'>('guildmarks');
-
-  // Set favicon
   useEffect(() => {
     const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
     link.type = 'image/png';
@@ -15,13 +11,5 @@ export default function App() {
     document.getElementsByTagName('head')[0].appendChild(link);
   }, []);
 
-  return (
-    <>
-      {currentPage === 'ledger' ? (
-        <BlacksmithLedgerPage onNavigate={setCurrentPage} />
-      ) : (
-        <GuildMarkPage onNavigate={setCurrentPage} />
-      )}
-    </>
-  );
+  return <GuildMarkPage />;
 }
